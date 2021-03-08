b819944d8675b032a31d10bfc8fe051c299583ee
Kent Auyeung <kentayeung@gmail.com>
2021-03-07 16:23:53 -0800

    Remove button. Added image and description on landing

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-const url = 'https://api.nasa.gov/planetary/apod?api_key=lYj89Zwnw8bd1ISjPVpMMlAiuzjwb3g3V6Cr5g4a&' + `${date}`
-let date = document.querySelector('.date').value
-
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-  })
-  .catch(err => {
-    console.log(`error ${err}`)
-  })
+const url = `https://api.nasa.gov/planetary/apod?api_key=lYj89Zwnw8bd1ISjPVpMMlAiuzjwb3g3V6Cr5g4a&date`
+  // let date = document.querySelector('.date').value = innerText
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      document.querySelector('img').src = data.hdurl
+      document.querySelector('p').innerText = data.explanation
+      // console.log()
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    })
