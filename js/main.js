d73f382b3b75d519310aa94204bf2243e1ed8d45
Kent Auyeung <kentayeung@gmail.com>
2021-03-07 16:50:14 -0800

    Layout edits

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 const url = `https://api.nasa.gov/planetary/apod?api_key=lYj89Zwnw8bd1ISjPVpMMlAiuzjwb3g3V6Cr5g4a&date`
   // let date = document.querySelector('.date').value = innerText
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      document.querySelector('img').src = data.hdurl
-      document.querySelector('p').innerText = data.explanation
-      // console.log()
-    })
-    .catch(err => {
-      console.log(`error ${err}`)
-    })
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    document.querySelector('img').src = data.hdurl
+    document.querySelector('a').src = data.hdurl
+    document.querySelector('h1').innerText = data.title
+    document.querySelector('p').innerText = data.explanation
+    // console.log()
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  })
